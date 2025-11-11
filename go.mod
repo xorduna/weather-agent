53535f03736974ad14b1ec2d8b2168c5bf5ba60a
Xavier Orduña <xavier.orduna@gmail.com>
2025-11-11 21:57:54 +0100

    Fixed title generation

--- go.mod
+++ go.mod
@@ -21,7 +21,7 @@ require (
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
-	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirectgo run
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
--- internal/chat/assistant/assistant.go
+++ internal/chat/assistant/assistant.go
@@ -29,11 +29,12 @@ func (a *Assistant) Title(ctx context.Context, conv *model.Conversation) (string
 
 	slog.InfoContext(ctx, "Generating title for conversation", "conversation_id", conv.ID)
 
-	msgs := make([]openai.ChatCompletionMessageParamUnion, len(conv.Messages))
+	msgs := []openai.ChatCompletionMessageParamUnion{
+		openai.SystemMessage("Generate a concise, descriptive title for the conversation. It should reflect users intention based on the user message. The title should be a single line, no more than 80 characters, and should not include any special characters or emojis."),
+	}
 
-	msgs[0] = openai.AssistantMessage("Generate a concise, descriptive title for the conversation based on the user message. The title should be a single line, no more than 80 characters, and should not include any special characters or emojis.")
-	for i, m := range conv.Messages {
-		msgs[i] = openai.UserMessage(m.Content)
+	for _, m := range conv.Messages {
+		msgs = append(msgs, openai.UserMessage(m.Content))
 	}
 
 	resp, err := a.cli.Chat.Completions.New(ctx, openai.ChatCompletionNewParams{
@@ -56,6 +57,8 @@ func (a *Assistant) Title(ctx context.Context, conv *model.Conversation) (string
 	if len(title) > 80 {
 		title = title[:80]
 	}
+
+	slog.InfoContext(ctx, "Generated title for conversation", "conversation_id", conv.ID, "title", title)
 
 	return title, nil
 }
